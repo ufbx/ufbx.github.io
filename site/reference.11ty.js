76fbbf51d242dd72e0d405d7ffaaf9789f6cbdc6
bqqbarbhg <samuli.1995@hotmail.com>
2022-01-15 00:56:05 +0200

    Allow multiple brackets in links

--- site/reference.11ty.js
+++ site/reference.11ty.js
@@ -6,7 +6,7 @@ const md = new MarkdownIt({
 })
 
 function linkRefs(str) {
-    return str.replace(/(ufbx_[A-Za-z0-9_\.]+)[A-Za-z0-9_/]*(\[[^\]]*\])?(\([^)]*\))?/, (sub, root) => {
+    return str.replace(/(ufbx_[A-Za-z0-9_\.]+)[A-Za-z0-9_/]*(\[[^\]]*\])*(\([^)]*\))?/, (sub, root) => {
         return `<a href="#${root}">${sub}</a>`
     })
 }
